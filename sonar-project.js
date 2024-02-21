26d2f4fd2645615f023974c82764eda1ba375805
pradeepasavai <155220366+pradeepasavai@users.noreply.github.com>
2024-02-21 12:59:00 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.201.97.206:9000/',
+  serverUrl: 'http://172.31.33.223:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
